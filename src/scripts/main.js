c1977c98a13bffbecf55ae1c721cac154027a933
m0x72 <m0x72c@gmail.com>
2015-03-07 18:41:46 +0100

    Container mount

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,7 +13,7 @@ var Router = require('react-router');
 var routes = require('./routes.js');
 
 Router.run(routes, Router.HistoryLocation, function (Handler) {
-      React.render(<Handler/>, document.body);
+      React.render(<Handler/>, document.querySelector('#main'));
 });
 
 //React.render(<App />, document.getElementById('main'));
