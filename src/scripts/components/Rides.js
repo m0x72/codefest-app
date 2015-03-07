74a9b9b7165b2c8ae1e77f5a1e288a07b89d6a04
m0x72 <m0x72c@gmail.com>
2015-03-07 19:00:22 +0100

    example requests

--- src/scripts/components/Rides.js
+++ src/scripts/components/Rides.js
@@ -1,11 +1,24 @@
 var React = require('react');
 
 var Rides = React.createClass({
+    loadRidesFromServer: function() {
+        // make request, set once finished
+        setTimeout(function() {
+            this.setState({rides: [{name: 'Peter'}]});
+        }.bind(this));
+    },
+    getInitialState: function () {
+        return {rides: []};
+    },
+    componentDidMount: function () {
+        var from, to, datetime;
+        this.loadRidesFromServer(from, to, datetime);
+    },
     render: function (){
         return (
             <div className="ride_list">
                 <ul>
-                    <li>Buh</li>
+                    <li>Name: {this.state.rides}</li>
                     <li>Buh2</li>
                 </ul>
             </div>
